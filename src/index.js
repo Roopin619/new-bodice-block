b7e074a89f6e2b0ca732c9d8eaa9052258ec03a1
Roopin Bhadri <bhadriroopin748@gmail.com>
2020-05-23 18:01:22 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import freesewing from '@freesewing/core'
 import plugins from '@freesewing/plugin-bundle'
 import config from '../config'
-import draftBox from './box'
+//Parts
+import draftArmhole from './armhole'
+import draftNeck from './neck'
+import draftFront from './front'
 
 // Create new design
 const Pattern = new freesewing.Design(config, plugins)
 
 // Attach the draft methods to the prototype
-Pattern.prototype.draftBox = draftBox
+Pattern.prototype.draftArmhole = draftArmhole
+Pattern.prototype.draftNeck = draftNeck
+Pattern.prototype.draftFront = draftFront
 
 export default Pattern
